2dccbc70f435b6467b4966ca11acf56a64c75573
Empty <emptypageworking@gmail.com>
2026-09-18 23:45:01 +0700

    feat(init): MVVM

---
+++ Calculator/Calculator/Application/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Valera on 18.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Calculator()
+        }
+    }
+}
---
+++ Calculator/Calculator/View/Calculator.swift
@@ -0,0 +1,18 @@
+//
+//  Calculator.swift
+//  Calculator
+//
+//  Created by Valera on 18.09.2026.
+//
+
+import SwiftUI
+
+struct Calculator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Calculator()
+}
